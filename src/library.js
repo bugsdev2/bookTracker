029ace00504e5f169a874b189b216b269448e21d
BugsLaptop <msherlock246@gmail.com>
2023-08-10 00:38:41 +0530

    Added prototype function to check read status and divided the big function into smaller functions

--- src/library.js
+++ src/library.js
@@ -5,40 +5,53 @@ export const library = () => {
 	
 	let myLibrary = [];
 
-	function Book(title, author, currentPage, pages, readStatus) {
+	// book constructor
+	function Book(title, author, currentPage, pages) {
 		this.title = title;
 		this.author = author;
 		this.currentPage = currentPage;
 		this.pages = pages;
-		this.readStatus = readStatus;
 	}
 	
-	const persepolis = new Book('Persepolis', 'Marjane Satrapi', 343, 343, 'completed');
-	const theSellout = new Book('The Sellout', 'Paul Beatty', 289, 289, 'completed');
-	const oneHundredYears = new Book('One Hundred Years of Solitude', 'Gabriel Garcia Marquez', 132, 336, 'reading');
-	const republicOfCaste = new Book('Republic of Caste', 'Anand Teltumbde', 91, 822, 'reading');
-	
-	function addBookToLibrary(e) {
-		e.preventDefault();
-		const formData = new FormData(e.target);
-		const formDataObj = {};
-		formData.forEach((value, key) => formDataObj[key] = value);
-		let readStatus = '';
-		if (formDataObj.current == formDataObj.pages) {
-			readStatus = 'completed';
+	// prototype function to check readStatus
+	Book.prototype.readStatus = function(){
+		if (this.currentPage == this.pages) {
+			return 'Completed';
 		} else {
-			readStatus = 'reading';
+			return Math.round((this.currentPage/this.pages)*100);
 		}
-		
+	}
+	
+	// function to push books to myLibary array
+	function pushToLibrary(formData) {
+		const formDataObj = Object.fromEntries(formData.entries());
 		const newBook = new Book(
 							formDataObj.title,
 							formDataObj.author,
 							formDataObj.current,
-							formDataObj.pages,
-							readStatus
+							formDataObj.pages
 		);
+		newBook['status'] = newBook.readStatus();
 		myLibrary.push(newBook);
 		console.log(myLibrary);
+	};
+	
+	// function to create element and append to the DOM
+	function test() {
+		
+	};
+	
+	//~ const book1 = new Book('Persepolis', 'Marjane Satrapi', 343, 343, 'completed');
+	//~ const book2 = new Book('The Sellout', 'Paul Beatty', 289, 289, 'completed');
+	//~ const book3 = new Book('One Hundred Years of Solitude', 'Gabriel Garcia Marquez', 132, 336, 'reading');
+	//~ const book4 = new Book('Republic of Caste', 'Anand Teltumbde', 91, 822, 'reading');
+	
+	//~ myLibrary.push(book1, book2, book3, book4);
+	
+	function addBookToLibrary(e) {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		pushToLibrary(formData);
 	};
 	
 	form.addEventListener('submit', addBookToLibrary);
